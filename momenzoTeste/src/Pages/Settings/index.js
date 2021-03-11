50f7b65f51bccb6ca01ba27f7e2937fb639374fa
Arley Souto <macx15@hotmail.com>
2021-03-11 07:17:46 -0300

    Adicionado Picker e Switchers

--- momenzoTeste/src/Pages/Settings/index.js
+++ momenzoTeste/src/Pages/Settings/index.js
@@ -1,10 +1,72 @@
-import React from 'react'
-import {View, Text, SafeAreaView} from 'react-native';
+import React, { useState } from 'react'
+import { Picker } from '@react-native-picker/picker';
+import { View, Text, SafeAreaView, StyleSheet, Switch } from 'react-native';
 
-export default function Settings(){
-    return(
-        <SafeAreaView>
-            <Text>Settings</Text>
+export default function Settings() {
+    const [qualidade, setQualidade] = useState();
+    const [estabilidade, setEstabilidade] = useState(false)
+    const [dicas, setDicas] = useState(false)
+
+    const estabili = () => setEstabilidade(previousState => !previousState)
+    const dicasVideo = () => setDicas(previousState => !previousState)
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.body}>
+                <Text style={styles.text}>Qualidade do Video</Text>
+                <Picker
+                    style={styles.picker}
+                    selectedValue={qualidade}
+                    onValueChange={(itemValue, itemIndex) =>
+                        setQualidade(itemValue)
+                    }>
+                    <Picker.Item label="780" value="780" />
+                    <Picker.Item label="1080" value="1080" />
+                </Picker>
+
+                <View style={{ flexDirection: 'row' }}>
+                    <Text style={styles.text}>Estabilazação de Video</Text>
+                    <Switch
+                        style={{ marginLeft: '25%', marginTop: '2%' }}
+                        onValueChange={estabili}
+                        value={estabilidade}
+                    />
+                </View>
+
+                <View style={{ flexDirection: 'row' }}>
+                    <Text style={styles.text}>Mostrar dicas de Video</Text>
+                    <Switch
+                        style={{ marginLeft: '25%', marginTop: '2%' }}
+                        onValueChange={dicasVideo}
+                        value={dicas}
+                    />
+                </View>
+            </View>
         </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#000'
+    },
+    text: {
+        color: '#FFF',
+        margin: 10,
+        fontSize: 20
+    },
+    body: {
+        marginTop: '5%',
+        paddingLeft: '3%'
+    },
+    picker: {
+        backgroundColor: '#FFF',
+        width: '17%',
+        height: '20%',
+        margin: 20,
+        justifyContent: 'center',
+        marginTop: '5%',
+        borderRadius: 6
+    }
+})
--- momenzoTeste/src/Pages/Videos/index.js
+++ momenzoTeste/src/Pages/Videos/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import {View, SafeAreaView, Text} from 'react-native';
+import { View, SafeAreaView, Text, StyleSheet } from 'react-native';
 
-export default function Videos(){
-    return(
-        <SafeAreaView>
-            <View>
-                <Text>Videos</Text>
+export default function Videos() {
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.body}>
+                <Text style={styles.text}>Não há videos em sua Galeria</Text>
             </View>
         </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#000',
+    },
+    text: {
+        color: '#FFF',
+        fontSize:20
+    },
+
+    body:{
+        justifyContent:'center',
+        alignItems:'center',
+        display:'flex',
+        marginTop:'80%'
+    }
+})
