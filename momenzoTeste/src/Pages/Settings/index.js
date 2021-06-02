1c07468b07ec0c70d3f27f0cdf2d077ac5538195
Arley Souto <macx15@hotmail.com>
2021-06-02 04:19:36 -0300

    Ajustes em Setting para salvar dados no AsyncStorage e Redux

--- momenzoTeste/src/Pages/Settings/index.js
+++ momenzoTeste/src/Pages/Settings/index.js
@@ -3,38 +3,85 @@ import { Picker as RNPickerSelect } from '@react-native-picker/picker';
 import { PickerView } from './styles';
 import { View, Text, SafeAreaView, StyleSheet, Switch } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux'
-import { resolutionCam, selectedRes } from '../../Redux/ConfigCam/action'
+import { resolutionCam, estabilidadeCam, dicasVideosStatus } from '../../Redux/ConfigCam/action'
 import AsyncStorange from '@react-native-async-storage/async-storage';
 
 export default function Settings() {
-    const [qualidade, setQualidade] = useState();
-    const [estabilidade, setEstabilidade] = useState(false)
-    const [dicas, setDicas] = useState(false)
+    //Constantes alocados no AsyncStorage
+    const RESOLUTION = 'resolution';
+    const ESTABILIZATION_VIDEO = 'estabilizar';
+    const DICAS = 'dicas'
+
+
+    const [dicas, setDicas] = useState(false);
+
     const dispatch = useDispatch()
 
-    const settingCam = useSelector((state) => state.resolutionCam.resolutionCam)
-  
+    const settingCam = useSelector((state) => state.resolutionCam.resolutionCam)// Dados de resolução  salvos no Redux
+    const estabilidadeCamera = useSelector((state) => state.resolutionCam.estabilidadeCam)//Dados de estabilidade salvos no Redux
+    const dicasVideos = useSelector((state) => state.resolutionCam.dicasVideosStatus)//Dados de dicas salvos no Redux
 
 
-    const estabili = () => setEstabilidade(previousState => !previousState)
-    const dicasVideo = () => setDicas(previousState => !previousState)
 
-    useEffect(() => {
-        //incia com o valor salvo no asyncStorage
-        async function loadQualidadeVideo(){
-            const storageResolution = await AsyncStorange.getItem('resolution')
-            
-            if(storageResolution){
-               dispatch(resolutionCam(storageResolution))
-            }
+    //switch de estabilidade de video
+    async function estabilization() {
+        await AsyncStorange.setItem(ESTABILIZATION_VIDEO, JSON.stringify(!estabilidadeCamera))
+    }
+
+
+    //switch de Dicas na gravação do video
+   async function dicasVideo(){
+       console.log(!dicasVideos)
+        try{
+            await AsyncStorange.setItem(DICAS, JSON.stringify(!dicasVideos))
         }
-        loadQualidadeVideo()
-    }, [])
+        catch(e){
+            console.log('Erro ao salvar dicas no Async Storage', e)
+        }
+
+    }
 
     //salvar a qualidade no asyncStorage
-    async function qualidadeVideo(data){
-        await AsyncStorange.setItem('resolution', settingCam)
+    async function qualidadeVideo() {
+        await AsyncStorange.setItem(RESOLUTION, settingCam)
     }
+
+    useEffect(() => {
+
+        //inicia as dicas salvas na memoria
+        async function dicasSalvas(){
+            const dicas = await AsyncStorange.getItem(DICAS)
+
+            if(dicas){
+                dispatch(dicasVideosStatus(JSON.parse(dicas)))
+                
+            }
+        }
+
+
+        //inicia com switch de estabilidade de video salvo na memoria
+        async function ajustesEstabilization() {
+            const estabi = await AsyncStorange.getItem(ESTABILIZATION_VIDEO);
+            if (estabi) {
+                dispatch(estabilidadeCam(JSON.parse(estabi)))
+                
+            }
+    
+        }
+        //incia com o valor salvo no asyncStorage
+        async function ajustesCamera() {
+            const storageResolution = await AsyncStorange.getItem(RESOLUTION);
+
+            if (storageResolution) {
+                dispatch(resolutionCam(storageResolution));
+
+            }
+        }
+        ajustesCamera()
+        dicasSalvas()
+        ajustesEstabilization()
+    }, [])
+
 
     return (
         <SafeAreaView style={styles.container}>
@@ -58,8 +105,8 @@ export default function Settings() {
                     <Text style={styles.text}>Estabilazação de Video</Text>
                     <Switch
                         style={{ marginLeft: '25%', marginTop: '2%' }}
-                        onValueChange={estabili}
-                        value={estabilidade}
+                        onValueChange={(e) => estabilization(dispatch(estabilidadeCam(e)))}
+                        value={estabilidadeCamera}
                     />
                 </View>
 
@@ -67,8 +114,8 @@ export default function Settings() {
                     <Text style={styles.text}>Mostrar dicas de Video</Text>
                     <Switch
                         style={{ marginLeft: '25%', marginTop: '2%' }}
-                        onValueChange={dicasVideo}
-                        value={dicas}
+                        onValueChange={(e) => dicasVideo(dispatch(dicasVideosStatus(e)))}
+                        value={dicasVideos}
                     />
                 </View>
             </View>
